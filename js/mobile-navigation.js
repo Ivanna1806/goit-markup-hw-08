0d89e333fdbaf730048c5050cf80f45ff47de24f
Ivanna1806 <105160306+Ivanna1806@users.noreply.github.com>
2022-07-24 22:59:58 +0300

    add08

--- js/mobile-navigation.js
+++ js/mobile-navigation.js
@@ -1,22 +1,9 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-(() => {
-  const refs = {
     openNavBtn: document.querySelector('[nav-btn]'),
     dropdown: document.querySelector('[dropdown]'),
     mobileNavWrap: document.querySelector('[mobile-nav-wrap]'),
+    body: document.querySelector('[body]'),
   };
 
   refs.openNavBtn.addEventListener('click', toggleModal);
@@ -25,5 +12,6 @@
     refs.dropdown.classList.toggle('hidden-dropdown');
     refs.openNavBtn.classList.toggle('opened');
     refs.mobileNavWrap.classList.toggle('active');
+    refs.body.classList.toggle('noscroll');
   }
 })();
